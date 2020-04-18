573e6e947c65341b9baeb93cb35b598e4e4ef0d9
daiyun.chen <daiyun.chen@lie360.com>
2020-04-18 16:55:30 +0800

    add

---
+++ public/data.js
@@ -0,0 +1,9 @@
+const data = {
+   //nodeConfig.type 0 起始节点 1审核节点 2抄送人节点 4分支节点
+
+   //flowPermission 流程发起人列表数据
+
+   //settype 审核节点的审核人设置  1 指定成员 2 主管 4 发起人自选 5 发起人自己 7连续多级主管
+
+   //examineMode  1一次审批 2 多人会签
+}
---
+++ src/plugins/preload.js
@@ -0,0 +1,43 @@
+function All() {}
+All.prototype = {
+    timer: "",
+    debounce(fn, delay = 500) {
+        var _this = this;
+        return function(arg) {
+            //获取函数的作用域和变量
+            let that = this;
+            let args = arg;
+            clearTimeout(_this.timer) // 清除定时器
+            _this.timer = setTimeout(function() {
+                fn.call(that, args)
+            }, delay)
+        }
+    },
+    setCookie(val) { //cookie设置[{key:value}]、获取key、清除['key1','key2']
+        for (var i = 0, len = val.length; i < len; i++) {
+            for (var key in val[i]) {
+                document.cookie = key + '=' + encodeURIComponent(val[i][key]) + "; path=/";
+            }
+        }
+    },
+    getCookie(name) {
+        var strCookie = document.cookie;
+        var arrCookie = strCookie.split("; ");
+        for (var i = 0, len = arrCookie.length; i < len; i++) {
+            var arr = arrCookie[i].split("=");
+            if (name == arr[0]) {
+                return decodeURIComponent(arr[1]);
+            }
+        }
+    },
+    clearCookie(name) {
+        var myDate = new Date();
+        myDate.setTime(-1000); //设置时间    
+        for (var i = 0, len = name.length; i < len; i++) {
+            document.cookie = "" + name[i] + "=''; path=/; expires=" + myDate.toGMTString();
+        }
+    },
+}
+window.func = new All();
+
+export default func;
